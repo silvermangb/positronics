c9294daf796e18bab7f815375f46dbc06cd4b3aa
Greg Silverman <Greg Silverman@gbshpwindows7>
2014-12-12 19:09:34 -0800

    new style

--- main.js
+++ main.js
@@ -15,11 +15,15 @@ system.set_api_version ('0.2');
 system.api_version = '0.3';
 console.log(system.get_base_url());
 
-console.log(system.get_locations_url());
-console.log(system.get_location_tickets_url('l0'));
-console.log(system.get_location_table_url('l0','t0'));
-console.log(system.get_location_ticket_url('l0','t1'));
-console.log(system.get_location_menu_url('l0'));
-console.log(system.get_location_tables_url('l0'));
-console.log(system.get_location_employees_url('l0'));
-console.log(system.get_location_employee_url('l0','e0'));
+//console.log(system.get_locations_url());
+//console.log(system.get_location_tickets_url('l0'));
+//console.log(system.get_location_table_url('l0','t0'));
+//console.log(system.get_location_ticket_url('l0','t1'));
+//console.log(system.get_location_menu_url('l0'));
+//console.log(system.get_location_tables_url('l0'));
+//console.log(system.get_location_employees_url('l0'));
+//console.log(system.get_location_employee_url('l0','e0'));
+
+console.log(system.gen_urls());
+
+console.log('exit');
