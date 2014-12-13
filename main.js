2536ab68c873aaac1c4f162132e2b379984c3582
Greg Silverman <Greg Silverman@gbshpwindows7>
2014-12-12 17:07:32 -0800

    x

--- main.js
+++ main.js
@@ -14,3 +14,12 @@ console.log(system.get_base_url());
 system.set_api_version ('0.2');
 system.api_version = '0.3';
 console.log(system.get_base_url());
+
+console.log(system.get_locations_url());
+console.log(system.get_location_tickets_url('l0'));
+console.log(system.get_location_table_url('l0','t0'));
+console.log(system.get_location_ticket_url('l0','t1'));
+console.log(system.get_location_menu_url('l0'));
+console.log(system.get_location_tables_url('l0'));
+console.log(system.get_location_employees_url('l0'));
+console.log(system.get_location_employee_url('l0','e0'));
