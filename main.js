bd181b85d1e7cb217cb32925f968835f0e08a9d1
Greg Silverman <Greg Silverman@gbshpwindows7>
2014-12-13 20:13:46 -0800

    use literals

--- main.js
+++ main.js
@@ -16,37 +16,20 @@ var account = require('account');
 //system.api_version = '0.3';
 //console.log(system.get_base_url());
 
-//console.log(system.get_locations_url());
-//console.log(system.get_location_tickets_url('l0'));
-//console.log(system.get_location_table_url('l0','t0'));
-//console.log(system.get_location_ticket_url('l0','t1'));
-//console.log(system.get_location_menu_url('l0'));
-//console.log(system.get_location_tables_url('l0'));
-//console.log(system.get_location_employees_url('l0'));
-//console.log(system.get_location_employee_url('l0','e0'));
 
-//console.log(system.gen_urls());
-//console.log(0+system.get_location_tickets_url('l0'));
-//console.log(1+system.get_location_ticket_url('l0','t0'));
-//console.log(1+system.get_location_ticket_items_url('l0','t0'));
-//console.log(1+system.get_location_ticket_item_id_url('l0','t0','i0'));
-
-
-var location_id = system.location_id;
-var ticket_id = system.ticket_id;
-var ticket_item_id = system.ticket_item_id;
-
-console.log(system.get_location_ticket({location_id:'l0',ticket_id:'t0',items:true,ticket_item_id:'tii'}));
+var x;
+x = system.get_location_ticket({'{location_id}':'l0','{ticket_id}':'t0','items':true,'{ticket_item_id}':'tii'});
+console.log(x);
 console.log(system.get_location_ticket(system.gen_ticket_args('l0','t0')));
 
-var args = system.gen_ticket_args('l0');
-
+var args = system.gen_ticket_args('l0','t0');
 
 console.log(system.get_location_ticket(args));
+
 delete args.items;
 for(var i=0;i<8;++i) {
 	var t = 't'+i;
-	args[ticket_id] = t;
+	args['{ticket_id}'] = t;
 	console.log(system.get_location_ticket(args));
 }
 
